8a08fdc6bb2c1fccb3b01cbaa7e632257d6f8cb9
crypto-caesar <113930650+crypto-caesar@users.noreply.github.com>
2023-09-28 10:01:00 -0400

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.7;
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
 
@@ -12,7 +10,7 @@ contract SimpleStorage {
         uint256 favoriteNumber;
         string name;
     }
-    // uint256[] public anArray;
+
     People[] public people;
 
     mapping(string => uint256) public nameToFavoriteNumber;
